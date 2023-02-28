52a3cfa4d7e93fbee28f43cb85cfd2b514386bd5
b-capra <caprabmc@gmail.com>
2023-02-28 18:48:28 -0500

    Create landing page, add font/logo assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      Emu for Vocab
+      <img className="Logo" src='src/assets/emu-logo.png' />
+      <h1>Meet Emu - Your new AI study buddy</h1>
+      <p>Emu is here to help study vocabulary in a more practical way than traditional flashcards.</p>
+      <p>Begin by logging in and adding words to your first list to see how Emu can help you learn!</p>
     </div>
   )
 }
